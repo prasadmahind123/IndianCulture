97d51b0281b1fc4f452bf2db44fccdd4f06dc60f
prasadmahind123 <163630263+prasadmahind123@users.noreply.github.com>
2025-04-14 21:50:36 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,34 +3,12 @@ import './Home.css';
 import NavBar from '../Components/NavBar';
 import { ChevronRight, MapPin } from "lucide-react"
 import Footer from '../Components/Footer';
-// import ImageSlider from '../Components/ImageSlider';
-// import SetDate from '../Components/SetDate.jsx';
-// import Typewriter from 'typewriter-effect';
+
 import {  useNavigate } from 'react-router-dom';
-
-//  import ScrollUp from '../Components/ScrollUp.jsx';
-
 const Home = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
-  // const handleMusic = () => {
-  //   navigate('/music');
-  // }
-  // const handleArch = () => {
-  //   navigate('/architecture');
-  // }
-  // const handleDance = () => {
-  //   navigate('/art');
-  // }
-  // const handleDress = () => {
-  //   navigate('/cultural');
-  // }
-  // const handleForts = () => {
-  //   navigate('/forts');
-  // }
-  // const handleLang = () => {
-  //   navigate('/language');
-  // }
+
   const handleHeritage = () => {
     navigate('/heritage');
   }
@@ -40,7 +18,6 @@ const Home = () => {
 
   useEffect(() => {
     const loadData = async () => {
-      // Simulate data loading
       await new Promise((resolve) => setTimeout(resolve, 1000));
       setIsLoading(false);
     };
@@ -65,123 +42,11 @@ const Home = () => {
       <div className="nav">
         <NavBar />
       </div>
-      {/* <div className="page1">
-        <div className="Slide-Img">
-          <ImageSlider />
-        </div>
-      </div>
-      <div className="page2">
-        <div className="page2-content">
-          <h2>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString('Traditional Music of India 🎶...!');
-                typewriter.start();
-              }}
-            />
-          </h2>
-          <div className="page-2-container">
-            <div className="page2-left">
-              <p>
-              ndia has a rich and diverse musical heritage that spans classical, folk, and devotional traditions. Indian music is deeply rooted in spirituality, culture, and storytelling, with distinct styles evolving across different regions.
-              </p>
-              <button className="learn-more">
-                <span className="circle" aria-hidden="true">
-                  <span className="icon arrow"></span>
-                </span>
-                <span className="button-text" onClick={handleMusic}>Explore</span>
-              </button>
-            </div>
-            <div className="page2-right">
-              <figure>
-                <img src="/images/Music/indian-1.jpg" alt="" />
-                <figcaption><a href="https://commons.wikimedia.org/wiki/File:An_Indian_classical_music_performance.jpg">Karen Blaha from Charlottesville, VA</a>, <a href="https://creativecommons.org/licenses/by-sa/2.0">CC BY-SA 2.0</a>, via Wikimedia Commons</figcaption>
-              </figure>
-            </div>
-          </div>
-        </div>
-      </div>
-        <ScrollUp />
-      <div className="set-date-container">
-        <SetDate />
-      </div>
-      <div className="page3">
-        <div className="page3-content">
-          <h2>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString('✅ Architectural Marvels in India...!');
-                typewriter.start();
-              }}
-            />
-          </h2>
-          <div className="page-3-container">
-            <div className="page3-left">
-              <img src="/images/BG/page3-1.webp" alt="" />
-            </div>
-            <div className="page3-right">
-              <p>
-                India is architectural heritage is a reflection of its rich history,
-                cultural diversity, and artistic excellence. From grand temples and
-                majestic forts to intricate cave carvings and colonial-era
-                structures, Indian architecture tells the story of ancient
-                civilizations, mighty empires, and spiritual traditions.
-              </p>
-              <button className="learn-more">
-                <span className="circle" aria-hidden="true">
-                  <span className="icon arrow"></span>
-                </span>
-                <span className="button-text" onClick={handleArch}>Explore</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="page4">
-        <div className="page4-content">
-          <h2>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString('✅ Explore more...!');
-                typewriter.start();
-              }}
-            />
-          </h2>
-            <div className="home-card">
-                <p><span><img src="/images/Languages/language-1.png" alt="" onClick={handleLang} /> 
-                <div className="home-card-text">
-                  <p>Language</p>
-                </div></span></p>
-                <p><span><img src="/images/dress.webp" alt="" onClick={handleDress} />
-                <div className="home-card-text">
-                  <p>Traditional Dress</p>
-                </div></span></p>
-                <p><span><img src="/images/Music/Folk-1.jpg" alt="" onClick={handleMusic} />
-                <div className="home-card-text">
-                  <p>Traditional Music</p>
-                </div></span></p>
-                <p><span><img src="/images/forts/jaislmer.webp" alt="" onClick={handleForts} />
-                <div className="home-card-text">
-                  <p>Forts</p>
-                </div></span></p>
-                <p><span><img src="/images/Dance/Lavani-1.jpg" alt="" onClick={handleDance} />
-                <div className="home-card-text">
-                  <p>Traditional Dance</p>
-                </div></span></p>
-          </div>
-          
-        </div>
-      </div> */}
+     
       
        <main className="main">
         <section className="hero-section">
           <div className="hero-background">
-            {/* <img
-              src="images/TajMahal.webp"
-              alt="Indian Heritage Background"
-              className="hero-image"
-            /> */}
             <div className="hero-overlay" />
           </div>
           <div className="container hero-container">
@@ -236,7 +101,7 @@ const Home = () => {
               {[
                 {
                   title: "Classical Dance",
-                  description: "From Bharatanatyam to Kathak, explore India's classical dance forms",
+                  description: "Bharatanatyam to Kathak, explore India's classical dance forms",
                   image: "images/Dance/Kathakali-1.jpg",
                   path : "/art"
                 },
@@ -248,7 +113,7 @@ const Home = () => {
                 },
                 {
                   title: "Spirituality & Yoga",
-                  description: "Inner peace, self-awareness, mindfulness, balance, and enlightenment.",
+                  description: "Inner peace, self-awareness, mindfulness, and enlightenment.",
                   image: "/images/Yoga/banner-2.jpg",
                   path : "/yoga"
                 },
@@ -421,89 +286,6 @@ const Home = () => {
        
       </main>
       <Footer></Footer>
-      {/* <footer className="footer">
-        <div className="container footer-container">
-          <div className="footer-grid">
-            <div className="footer-column">
-              <Link href="/" className="logo">
-                <div className="logo-icon">
-                  <div className="logo-text">भा</div>
-                </div>
-                <span>भारतीय संस्कृति</span>
-              </Link>
-              <p className="footer-description">Celebrating and preserving the rich cultural heritage of India</p>
-            </div>
-            <div className="footer-column">
-              <h3 className="footer-heading">Quick Links</h3>
-              <ul className="footer-links">
-                <li className="footer-link" >
-                    Home
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    About Us
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Events
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Gallery
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h3 className="footer-heading">Explore</h3>
-              <ul className="footer-links">
-                <li>
-                  <Link href="#" className="footer-link">
-                    Classical Arts
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Festivals & Celebrations
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Heritage Sites
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Traditional Crafts
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="footer-link">
-                    Indian Cuisine
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h3 className="footer-heading">Connect With Us</h3>
-              <div className="contact-info">
-                <p className="contact-text">Email: info@indianculture.org</p>
-                <p className="contact-text">Phone: +91 123 456 7890</p>
-              </div>
-            </div>
-          </div>
-          <div className="copyright">
-            <p>© {new Date().getFullYear()} Indian Culture & Heritage. All rights reserved.</p>
-          </div>
-        </div>
-      </footer> */}
 
     </div>
   );
