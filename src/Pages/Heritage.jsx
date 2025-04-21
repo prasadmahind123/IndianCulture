a8acc3cbe2b367e3c4d2e7c0f00811e95d0c0d1d
prasadmahind123 <163630263+prasadmahind123@users.noreply.github.com>
2025-04-21 22:37:35 +0530

    Update Heritage.jsx

--- src/Pages/Heritage.jsx
+++ src/Pages/Heritage.jsx
@@ -3,28 +3,11 @@ import  { useState, useEffect } from 'react';
 import './Heritage.css'
 import { Link } from 'react-router-dom';
 import { ChevronRight, Calendar, MapPin, Book, Info } from "lucide-react"
-// import { PiGreaterThanThin } from "react-icons/pi";
-// import Typewriter from 'typewriter-effect';
- import { useNavigate } from 'react-router-dom';
-// import ScrollUp from '../Components/ScrollUp.jsx';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer.jsx';
 export default function Heritage() {
    const navigate = useNavigate();
-    // const handleHome = () => {
-    //     navigate('/');
-    // }
-    // const handleCuisine = () => {
-    //     navigate('/cuisine')
-    // }
-    // const handleHeritage = () => {
-    //     navigate('/architecture')
-    // }
-    // const handleYoga = () => {
-    //     navigate('/yoga')
-    // }
-    // const handleArt = () => {
-    //     navigate('/art')
-    // }
+
       const [isLoading, setIsLoading] = useState(true);
     
       useEffect(() => {
@@ -52,171 +35,7 @@ export default function Heritage() {
             <NavBar />
         </div>
 
-        {/* <div className="page-1"> 
-        <div className="content-right">
-            <img src="/images/igate.webp" alt="" />
-        </div>
-            <div className="heading">               
-                <p ><span onClick={handleHome}>Home</span> <PiGreaterThanThin className='greater'/> Heritage</p>
-            </div>
-            
-            <div className="content">
-                <div className="content-left">
-                    <span className="t">Timeless Traditions, Eternal Legacy</span>
-                    <h1>Indian Heritage: A Legacy of Culture and Tradition...!</h1>
-
-                </div> 
-            </div>
-
-        </div>
-        <ScrollUp />
-        <div className="page-2">
-            <div className="heitageData">
-                <div className="heritagecard">
-                    <p>
-                        India&apos;s heritage is a vibrant tapestry woven with history, culture, traditions, and diversity. Spanning thousands of years, it reflects the influence of various dynasties, religions, and civilizations that have shaped the nation. From awe-inspiring monuments like the Taj Mahal, Ajanta & Ellora Caves, and the temples of Khajuraho to the majestic forts and palaces of Rajasthan, India architectural wonders stand as timeless symbols of its glorious past.
-                        The country is a spiritual hub, being the birthplace of Hinduism, Buddhism, Jainism, and Sikhism.
-                    </p>
-                </div>
-                <div className="heritagecard">
-                    <p>
-                        India, with its 5,000-year-old civilization, is home to some of the world&apos;s most iconic heritage sites. These places tell the story of ancient kingdoms, architectural brilliance, cultural diversity, and spiritual traditions. From grand palaces to sacred temples, every site reflects the essence of India&apos;s rich history.
-                        India’s heritage is a blend of history, architecture, nature, and traditions. From the magnificent Taj Mahal to the sacred temples of Hampi, and the forests of Kaziranga, these sites connect us to our past and inspire the future.
-                    </p>
-                </div>
-                <div className="heritagecard">
-                    <p>
-                        India’s architectural heritage is a blend of spiritual, royal, and artistic excellence. From the temples of South India to the forts of Rajasthan, from Mughal palaces to Buddhist caves, every structure tells a story of India’s glorious past. These landmarks continue to inspire architects, historians, and travelers from around the world.
-                        India&apos;s monuments and historical sites offer a deep dive into its royal heritage, architectural brilliance, and cultural diversity. These landmarks stand as symbols of India’s glorious past, preserving the stories of emperors, warriors, saints, and artisans.
-                    </p>
-                </div>
-            </div>
-            
-           
-           
-           
-            <div className="heading">
-                <h2>
-                    {
-                         <Typewriter
-                         onInit={(typewriter) => {
-                             typewriter.typeString('India’s Glorious Heritage Categorized into the following types  : ')
-                             .start();
-                         }}
-                      />
-                      }  
-                    </h2>
-            </div>
-            <div className="page-2-content">
-                <div className="page-2-left">
-    
-                   
-                    <h3>1.  Historic Monuments & Architecture</h3>
-                    <p>India is home to magnificent architectural wonders like the Taj Mahal, Qutub Minar, Sun Temple, and Ajanta-Ellora Caves, reflecting the artistic genius of our ancestors.</p>
-                    <h4>For example :</h4>
-                    <ul>
-                        <li>
-                            <p>1. UNESCO World Heritage Sites (Taj Mahal, Qutub Minar, Hampi, etc.)</p>
-                        </li>
-                        <li>
-                            <p>2. Temples, Forts, and Palaces (Khajuraho, Jaipur Palaces, Sun Temple)</p>
-                        </li>
-                    </ul>
-                    <button className="learn-more" onClick={handleHeritage}>
-                        <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                        </span>
-                        <span className="button-text">Explore</span>
-                  </button>
-                </div>
-                <div className="page-2-right">
-                    <img src="/images/Memorial.webp" alt="" />
-                </div>
-            </div>
-        </div>
-        
-        <div className="page-3">
-            <div className="page-3-content">
-                <div className="page-3-right">
-                        <img src="images/Paintings/warli-1.png" alt="" />
-                </div>
-                <div className="page-3-left">
-                    <h3>2.  Indian Art & Dance</h3>
-                    <p>India’s art&apos;s are centuries old, deeply rooted in its diverse cultures and regional influences. They reflect the country’s history, mythology, and daily life.</p>
-                    <h4>For example :</h4>
-                    <ul>
-                        <li>
-                            <p>1. Traditional Paintings</p>
-                        </li>
-                        <li>
-                            <p>2. Classical Dance Forms (Bharatanatyam, Kathak, Odissi)</p>
-                        </li>
-                    </ul>
-                    <button className="learn-more">
-                        <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                        </span>
-                        <span className="button-text" onClick={handleArt}>Explore</span>
-                    </button>
-                </div>
-        
-            </div>
-        </div>
-        <div className="page-2">
-            <div className="page-2-content">
-                <div className="page-2-left">
-                    <h3>3.  Indian Cuisine</h3>
-                    <p>Indian cuisine is diverse, flavorful, and deeply rooted in tradition. It varies by region, influenced by geography, climate, and history.</p>
-                    <p>Indian cuisine blends tradition, taste, and health, making it one of the most loved cuisines worldwide! 🍽️</p>
-                    <h4>For example :</h4>
-                    <ul>
-                        <li>
-                            <p>1. Tamil Nadu – Dosa 🥞🥥</p>
-                        </li>
-                        <li>
-                            <p>2. Maharashtra – Vada Pav 🍔🔥</p>
-                        </li>
-                    </ul>
-                    <button className="learn-more" onClick={handleCuisine}>
-                        <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                        </span>
-                        <span className="button-text">Explore</span>
-                  </button>
-                </div>
-                <div className="page-2-right">
-                    <img src="/images/HeritagePage/food.webp" alt="" />
-                </div>
-            </div>
-        </div>
-        <div className="page-3">
-            <div className="page-3-content">
-                <div className="page-3-right">
-                        <img src="/images/HeritagePage/yoga.webp" alt="" />
-                </div>
-                <div className="page-3-left">
-                    <h3>4.  Spirituality & Yoga</h3>
-                    <p>India, the birthplace of yoga and spirituality, has practiced meditation and self-realization for over 5,000 years. Yoga, mentioned in the Rigveda and systematized by Patanjali (200 BCE), promotes physical, mental, and spiritual well-being. Rishikesh and Varanasi are key spiritual centers, and June 21 is celebrated as International Yoga Day. 🕉️</p>
-                    <h4>For example :</h4>
-                    <ul>
-                        <li>
-                            <p>1. Role of Yoga & Meditation (Ayurveda, Vipassana, Patanjali)</p>
-                        </li>
-                        <li>
-                            <p>2. Spiritual Destinations (Varanasi, Rishikesh, Bodh Gaya)</p>
-                        </li>
-                    </ul>
-                    <button className="learn-more" onClick={handleYoga}>
-                        <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                        </span>
-                        <span className="button-text">Explore</span>
-                    </button>
-                </div>
-        
-            </div>
-            
-        </div> */}
+       
         <section className="hero">
             <div className="hero-content">
                 <h1>Discover India&apos;s Rich Heritage</h1>
@@ -240,50 +59,61 @@ export default function Heritage() {
         <div className="category-grid">
           <div className="category-card">
             <div className="category-image">
-              <img src="/images/Arcitecture/architecture.webp" alt="Monuments" width={400} height={300} />
+              <img src="/images/forts/red.webp" alt="Monuments" width={400} height={300} />
             </div>
-            <h3>Monuments & Architecture</h3>
-            <p>Ancient temples, forts, and palaces that showcase India&apos;s architectural brilliance</p>
-            <li className="read-more" onClick={() => navigate('/architecture')}>
-              Explore <ChevronRight className="icon" size={16} />
-            </li>
+            <div className="category-info">
+              <h3>Monuments & Architecture</h3>
+              <p>Ancient temples, forts, and palaces that showcase India&apos;s architectural brilliance</p>
+            </div>
+           
+            <div className="read-more">
+              Explore <ChevronRight className="icon" size={16} onClick={() => navigate('/architecture')} />
+            </div>
           </div>
 
           <div className="category-card">
             <div className="category-image">
               <img src="/images/Paintings/Tanjore-2.jpeg" alt="Art Forms" width={400} height={300} />
             </div>
-            <h3>Classical Art Forms</h3>
-            <p>Traditional dance, music, and theatrical performances passed down through generations</p>
-            <li className="read-more" onClick={() => navigate('/art')}>
-              Explore <ChevronRight className="icon" size={16}  />
-            </li>
+            <div className="category-info"> 
+              <h3>Classical Art Forms</h3>
+              <p>Explore India&apos;s classical art forms, including dance, music, and painting</p>
+            </div>
+            
+            <div className="read-more">
+              Explore <ChevronRight className="icon" size={16} onClick={() => navigate('/architecture')} />
+            </div>
           </div>
 
           <div className="category-card">
             <div className="category-image">
               <img src="/images/wild/Kaziranga.webp" alt="Handicrafts" width={400} height={300} />
             </div>
-            <h3>National Parks & Wildlife</h3>
-            <p>Explore Indian National Parks & Wildlife Sanctuaries, Where Conservation Comes Alive!</p>
-            <li className="read-more" onClick={() => navigate('/wild')}>
-              Explore <ChevronRight className="icon" size={16} />
-            </li>
+            <div className="category-info">
+              <h3>National Parks & Wildlife</h3>
+              <p>Discover India&apos;s rich biodiversity and wildlife sanctuaries</p>
+            </div>
+            
+            <div className="read-more">
+              Explore <ChevronRight className="icon" size={16} onClick={() => navigate('/architecture')} />
+            </div>
           </div>
 
           <div className="category-card">
             <div className="category-image">
               <img src="/images/CuisinePage/dhokla1.jpeg" alt="Literature" width={400} height={300} />
             </div>
-            <h3>Indian Cuisine</h3>
-            <p>Healthy and Delicious : A Culinary Journey Through Flavors</p>
-            <li className="read-more" onClick={() => navigate('/cuisine')}>
-              Explore <ChevronRight className="icon" size={16}  />
-            </li>
+            <div className="category-info">
+              <h3>Indian Cuisine</h3>
+              <p>Explore the diverse flavors and culinary traditions of India</p>
+            </div>
+            
+            <div className="read-more">
+              Explore <ChevronRight className="icon" size={16} onClick={() => navigate('/architecture')} />
+            </div>
           </div>
         </div>
       </section>
-
       {/* Featured Heritage Site */}
       <section className="featured-site">
         <div className="featured-content">
